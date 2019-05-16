e024c27a94db3b10512a320395cb030ac5075333
Ivona <najdenkoskai@gmail.com>
2019-05-16 18:14:42 +0200

    Fixed image files ordering.

--- src/utils.py
+++ src/utils.py
@@ -9,10 +9,12 @@ PATH_TO_DATASET = str(Path.cwd().parent) + '/data/dataset/'
 
 def read_train_images():
     filenames = [img for img in glob.glob(PATH_TO_DATASET + "images/train/*.jpg")]
+    filenames.sort()
     return np.array([resize(io.imread(img_f), (IMAGE_SIZE, IMAGE_SIZE, 3)) for img_f in filenames]).astype('float32')
 
 def read_validation_images():
     filenames = [img for img in glob.glob(PATH_TO_DATASET + "images/validation/*.jpg")]
+    filenames.sort()
     return np.array([resize(io.imread(img_f), (IMAGE_SIZE, IMAGE_SIZE, 3)) for img_f in filenames]).astype('float32')
 
 def read_train_labels():
