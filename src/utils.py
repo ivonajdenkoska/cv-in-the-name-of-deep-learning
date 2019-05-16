761a6920ed7014f804a59899162e5fba0f481f9a
Teodora Popordanoska <teodorapopordanoska@yahoo.com>
2019-05-16 18:46:05 +0200

    Fix reading images

--- src/utils.py
+++ src/utils.py
@@ -8,14 +8,20 @@ from constants import IMAGE_SIZE
 PATH_TO_DATASET = str(Path.cwd().parent) + '/data/dataset/'
 
 def read_train_images():
+    images = []
     filenames = [img for img in glob.glob(PATH_TO_DATASET + "images/train/*.jpg")]
-    filenames.sort()
-    return np.array([resize(io.imread(img_f), (IMAGE_SIZE, IMAGE_SIZE, 3)) for img_f in filenames]).astype('float32')
+    for i in range(0, len(filenames)):
+        img_path = PATH_TO_DATASET + "images/train/img" + str(i) + ".jpg"
+        images.append(resize(io.imread(img_path), (IMAGE_SIZE, IMAGE_SIZE, 3)))
+    return np.array(images)
 
 def read_validation_images():
+    images = []
     filenames = [img for img in glob.glob(PATH_TO_DATASET + "images/validation/*.jpg")]
-    filenames.sort()
-    return np.array([resize(io.imread(img_f), (IMAGE_SIZE, IMAGE_SIZE, 3)) for img_f in filenames]).astype('float32')
+    for i in range(0, len(filenames)):
+        img_path = PATH_TO_DATASET + "images/validation/img" + str(i) + ".jpg"
+        images.append(resize(io.imread(img_path), (IMAGE_SIZE, IMAGE_SIZE, 3)))
+    return np.array(images)
 
 def read_train_labels():
     return np.loadtxt(fname=PATH_TO_DATASET + 'labels/y_train.txt', dtype=int, delimiter=",")
